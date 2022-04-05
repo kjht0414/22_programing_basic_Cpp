c26229dcfe893966d612d8da92f309f7d4e4ad08
kjht0414 <kjht0414@gmail.com>
2022-04-05 20:27:48 +0900

    3.23, 3.30

--- Test1/테스트1.c
+++ Test1/테스트1.c
@@ -85,7 +85,7 @@ int main() {//main function , int : integer (정수)
 	}*/
 
 	// 1부터 n까지의 누적합 구하기 (while version)
-	int n ,result=0 ,count=1;
+	/*int n ,result=0 ,count=1;
 	printf("값을 입력 : ");
 	scanf_s("%d", &n);
 
@@ -95,6 +95,81 @@ int main() {//main function , int : integer (정수)
 	}
 
 	printf("1부터 %d까지의 덧셈 : %d\n", n, result);
+*/
 
+
+	//array
+
+	//int a;
+	//int b[40]; //배열
+
+	//for (int i = 0; i < 40; i++) {
+	//	b[i] = i;
+	//}
+
+	//for (int i = 0; i < 40; i++) {
+	//	printf("%d\n", b[i]);
+	//}
+	
+	//누적합 배열 버전, 중간 합산을 배열에 보관
+	/*
+	int result[100];
+	int n;
+	for (int i = 0; i < 100; i++)
+		result[i] = 0;
+
+	printf("얼마까지 누적합을 원하세요? 원하시는 값을 입력하세요:");
+	scanf_s("%d", &n);
+	for (int i = 1; i < n;i++) {
+		result[i] += result[i-1] + i;
+	}
+	
+	for (int i = 1; i <= n; i++)
+		printf("1부터 %d까지의 누적합 : %d\n", i, result[i]);
+	*/
+
+	//연습문제 2022 03 30 구구단 곱셈 배열
+	
+	/*
+	int dan;
+	int result[9];
+	for (int i = 0; i < 9; i++) {
+		result[i] = 0;
+	}
+	printf("단값(2~9): ");
+	scanf_s("%d", &dan);
+
+
+
+	for (int i = 1; i <= 9; i++) {
+		//printf("%d X %d = %d\n",dan,i,dan*i);
+		result[i - 1] = dan * i;
+	}
+
+	for (int i = 1; i <= 9; i++) {
+		printf("%d X %d = %d\n", dan, i, result[i-1]);
+	}
+	*/
+
+	int dan;
+	int result[9];
+	int i=0;
+	while (i < 9) {
+		result[i] = 0;
+		i++;
+	}
+	printf("단값(2~9): ");
+	scanf_s("%d", &dan);
+
+	i = 1;
+	while (i <= 9) {
+		result[i - 1] = dan * i;
+		i++;
+	}
+	i = 1;
+	while (i <= 9) {
+		printf("%d X %d = %d\n", dan, i, result[i - 1]);
+		i++;
+	}
 }
 
