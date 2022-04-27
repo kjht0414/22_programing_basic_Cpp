ad93cb2033df5a31c21ea67c94da2ef7a5cb0e5b
kjht0414 <kjht0414@gmail.com>
2022-04-27 15:20:00 +0900

    성적관리 1-2 업대이트

--- 미션1/성적관리_프로그램v1-2.c
+++ 미션1/성적관리_프로그램v1-2.c
@@ -3,13 +3,12 @@
 버전을 2가지로 만들것 ex)첮번째 버전이 배열 없이 정수 및 실수 자료를 직접 대입하여
 처리하고 for 문, 함수 등으로 구현했다면
 두번째 버전은 scanf로 입력하고 while 문으로 함수없이 구현
-
 */
 
 /*
 * 학생 성적 처리 프로그램을 제작(common)
 * 학생수를 미리 지정하고 정보를 입력(version1)
-* ===================================================
+* ========================나중에 할것===========================
 * 과목을 커스텀 가능하도록(version2)
 * 학생수를 미리 지정하지 않고 동적으로 추가, 삭제, 삽입이 가능할것(version2)
 * 학생의 특정 속성(이름,번호,성적 등)으로 정렬할 수 있을것(version2)
@@ -18,6 +17,7 @@
 /*
 * while 문이 아니라 for 문으로 구현
 * 포인터를 배열로 변경
+* 장점: 더 보기 좋은 코드
 */
 
 #include<stdio.h>
@@ -32,20 +32,20 @@ int get_str_size(char* str) {//for 문으로 구현
 void str_copy(char* dst, char* src) {//for 문으로 구현
 	int count;
 	char c;
-	for (count = 0; c=src[count]; count++) {
+	for (count = 0; c = src[count]; count++) {
 		dst[count] = c;
 	}
 	dst[count] = c;
 }
 
-void student_inform_name(char** names,int index) {
+void student_inform_name(char** names, int index) {
 	char name_temp[0xFF];
 	int name_size;
 
 	scanf_s("%s", name_temp, 0xFF);
 
 	name_size = get_str_size(name_temp);
-	names[index] = malloc(sizeof(char) * (name_size + 1)); 
+	names[index] = malloc(sizeof(char) * (name_size + 1));
 	str_copy(names[index], name_temp);
 }
 
@@ -53,16 +53,15 @@ void student_inform_grade(int* grade) {
 	scanf_s("%d", grade);
 }
 
-void print_students(char** names, int* kor_grades, int* math_grades, int* eng_grades, float* avg_grades,int size) {
+void print_students(char** names, int* kor_grades, int* math_grades, int* eng_grades, float* avg_grades, int size) {
 	char* name;
-	printf("=========================\n");
 	for (int i = 0; i < size; i++) {
-		name = *(names + i);
+		name = names[i];
 		printf("이름: %s\n", name);
 		printf("국어: %d\n", kor_grades[i]);
 		printf("수학: %d\n", math_grades[i]);
 		printf("영어: %d\n", eng_grades[i]);
-		printf("평균: %.2f\n", eng_grades[i]);
+		printf("평균: %.2f\n", avg_grades[i]);
 		printf("=========================\n");
 	}
 }
@@ -77,7 +76,7 @@ int main() {
 	float* student_grades_avg;
 
 	printf("학생들의 수:");
-	scanf_s("%d",&student_size);
+	scanf_s("%d", &student_size);
 
 	student_names = malloc(sizeof(char*) * student_size);
 	student_grades_kor = malloc(sizeof(int) * student_size);
@@ -88,24 +87,25 @@ int main() {
 
 	for (int i = 0; i < student_size; i++) {
 		printf("(%d/%d)번째 학생의 이름:", i + 1, student_size);
-		student_inform_name(student_names,i);
+		student_inform_name(student_names, i);
 
 		printf("(%d/%d)번째 학생의 국어:", i + 1, student_size);
-		student_inform_grade(student_grades_math[i]);
+		student_inform_grade(&student_grades_kor[i]);
 
 		printf("(%d/%d)번째 학생의 수학:", i + 1, student_size);
-		student_inform_grade(student_grades_eng[i]);
+		student_inform_grade(&student_grades_math[i]);
 
 		printf("(%d/%d)번째 학생의 영어:", i + 1, student_size);
-		student_inform_grade(student_grades_kor[i]);
+		student_inform_grade(&student_grades_eng[i]);
 
-		for (int i = 0; i < student_size;i++) {
+		for (int i = 0; i < student_size; i++) {
 			student_grades_avg[i] = 0;
 			student_grades_avg[i] += student_grades_kor[i];
 			student_grades_avg[i] += student_grades_math[i];
 			student_grades_avg[i] += student_grades_eng[i];
 			student_grades_avg[i] /= 3;
 		}
+		printf("=========================\n");
 	}
 
 	print_students(student_names, student_grades_kor, student_grades_math, student_grades_eng, student_grades_avg, student_size);
